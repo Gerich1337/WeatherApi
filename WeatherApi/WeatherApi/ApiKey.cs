04d64f84fedb7ea3b00e9a57bd72ec4a3ded169f
AMGSex <maksimpetrakov25929@gmail.com>
2022-07-01 19:25:09 +0300

    Добавьте файлы проекта.

---
+++ WeatherApi/WeatherApi/ApiKey.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApi
+{
+    public class ApiKey
+    {
+        public static string key = "de99f446-a9ce-4c6e-90dd-020bec178ea6";
+    }
+}
